0fc46bb0c9835fa940b6189c39b9ff35dec04303
Leo Carvalho <carvalho.csleo@gmail.com>
2021-07-20 03:09:19 -0300

    Install route middlewares validators

--- src/routes/cepRoutes.js
+++ src/routes/cepRoutes.js
@@ -2,8 +2,14 @@
 const { Router } = require('express')
 const cepController = require('../controllers/cepController')
 
+const cepValidator = require('../validators/cepValidator')
+const validatorMiddleware = require('../middlewares/validatorMiddleware')
+
 const router = Router()
 
-router.get('/:cepNumber', cepController.getCep)
+router.get('/:cepNumber',
+  cepValidator,
+  validatorMiddleware,
+  cepController.getCep)
 
 module.exports = router
