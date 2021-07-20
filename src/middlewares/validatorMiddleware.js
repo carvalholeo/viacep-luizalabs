609a3e68a734fb0460b9c857272e633d84bea98c
Leo Carvalho <carvalho.csleo@gmail.com>
2021-07-19 23:19:00 -0300

    Create express-validator and too busy middlware

---
+++ src/middlewares/validatorMiddleware.js
@@ -0,0 +1,28 @@
+// @ts-check
+'use strict'
+
+const { validationResult } = require('express-validator')
+
+/**
+ * Function to handle with errors from Express Validator.
+ * @param {Request} req Object with the Request
+ * @param {Response} res Object to handle with response
+ * @param {Function} next Callback to be called if no errors occured.
+ * @return {Response} Returns with response object if a error were found.
+ */
+function validatorMiddleware (req, res, next) {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(406)
+      .json({
+        message: 'Your request is invalid. Here are the errors',
+        errors: errors.mapped()
+      })
+  }
+
+  next()
+}
+
+module.exports = validatorMiddleware
