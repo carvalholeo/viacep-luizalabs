b69053110b90b997d38f323b8e59ae7a788b58be
Leo Carvalho <carvalho.csleo@gmail.com>
2021-07-19 22:00:55 -0300

    Fix initial code to ESLint style

--- routes/values.js
+++ routes/values.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET values */
-router.get('/values', function(req, res, next) {
-  res.json(['value1', 'value2']);
-});
+router.get('/values', function (req, res, next) {
+  res.json(['value1', 'value2'])
+})
 
 /* GET values/5 */
-router.get('/values/:id', function(req, res, next) {
-  res.send('value');
-});
+router.get('/values/:id', function (req, res, next) {
+  res.send('value')
+})
 
 /* POST values */
-router.post('/values', function(req, res, next) {
-  res.send();
-});
+router.post('/values', function (req, res, next) {
+  res.send()
+})
 
 /* PUT values/5 */
-router.put('/values/:id', function(req, res, next) {
-  res.send();
-});
+router.put('/values/:id', function (req, res, next) {
+  res.send()
+})
 
 /* DELETE values/5 */
-router.delete('/values/:id', function(req, res, next) {
-  res.send();
-});
+router.delete('/values/:id', function (req, res, next) {
+  res.send()
+})
 
-module.exports = router;
+module.exports = router
